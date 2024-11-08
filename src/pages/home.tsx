88285f3b91c96818c0456d0f20efb52e2fe2706f
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-08 20:39:50 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Header />
       <main className="min-h-screen bg-background pt-32">
-        <div className="py-4 mx-24 my-12 bg-gradient-to-r from-primary via-secondary to-header rounded-xl">
+        <div className="py-4 mx-24 my-12 bg-gradient-to-r from-primary via-secondary to-header rounded-xl border-header border-4">
             <h1 className="text-4xl mx-28 my-6 text-background">Welcome Social Sync</h1>
         </div>
         
