95f6e217710bedca22e9cc638a25f3be59a62518
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-08 13:46:01 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div><h1>Home</h1><h2>Whats up?</h2></div>;
+  return <div>
+    <h1>Home</h1>
+    <h2>Whats up?</h2>
+    <select name="Select Option">
+      <option value="select1">Programming</option>
+      <option value="select2">Art</option>
+      <option value="select2">Mechanics</option>
+    </select>
+    
+  </div>;
 }
