37bc1e95bcbd497df274f2fb2b3041a80168406d
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-08 20:00:03 +0200

    Create home.tsx

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen bg-background pt-32">
+
+
+        
+      </main>
+    </div>
+  );
+}
