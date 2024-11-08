e5176a3a079ce56ec924add56f41a258a50da826
dragos1-5 <dragospopute@gmail.com>
2024-11-08 20:57:48 +0200

    Create contact.tsx
    x

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+
+export default function Contact(){
+    return(
+        <div>
+            <Header />
+            <main className="min-h-screen bg-background pt-32">
+        <div><h1 style={{fontSize:"40px"}}>Salut</h1></div>
+        </main>
+        </div>
+    )
+
+}
