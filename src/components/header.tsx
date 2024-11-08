3de3bc40e4355ed91de801e732ca1eb1881ae5a3
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2024-11-08 19:39:40 +0200

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,26 @@
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 h-[100px] bg-header">
-      <nav>
-        <title>Social Sync</title>
-        
+    <header className="fixed top-0 left-0 right-0 h-[100px] bg-header z-20 flex items-center justify-center">
+      <nav className="w-[1200px] h-full flex items-center justify-between">
+        <div>
+          <h1 className="text-background text-2xl font-bold">
+            Social<span className="text-primary">Sync</span>
+          </h1>
+        </div>
         <ul>
-          <div style={{display:"flex",alignItems:'center'}}>
-            <div style={{flexGrow: "1",color:"#FEF3E2",fontSize:"35px",fontStyle:"italic",float:"left",textAlign:'center'}}>SOCIAL SYNC</div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div
+              style={{
+                flexGrow: "1",
+                color: "#FEF3E2",
+                fontSize: "35px",
+                fontStyle: "italic",
+                float: "left",
+                textAlign: "center",
+              }}
+            >
+              SOCIAL SYNC
+            </div>
           </div>
         </ul>
       </nav>
