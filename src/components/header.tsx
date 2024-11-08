bc6910229f004abb1d1201ae4d2bc89d980230c9
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-08 17:31:54 +0200

    title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,12 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 h-[100px] bg-header">
       <nav>
+        <title>Social Sync</title>
+        
         <ul>
-          <li>test</li>
+          <div style={{display:"flex",alignItems:'center'}}>
+            <div style={{flexGrow: "1",color:"#FEF3E2",fontSize:"35px",fontStyle:"italic",float:"left",textAlign:'center'}}>SOCIAL SYNC</div>
+          </div>
         </ul>
       </nav>
     </header>
