4a6d81b7e88280376c9545241138cdbd125ce30e
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-08 18:48:03 +0200

    s

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen bg-background pt-32">
+        <div><h1 style={{fontSize:"40px"}}>Salut</h1></div>
+      </main>
+    </div>
+  );
+}
