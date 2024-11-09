b70ae55e7f1d9947b1f027ff7deb0e5f7f9502f6
dragos1-5 <dragospopute@gmail.com>
2024-11-09 14:18:01 +0200

    ho
    hom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,19 @@ export default function Home() {
       <Header />
 
       <main className="min-h-screen bg-background pt-32">
-                <div style={{ backgroundColor: 'orange', width: 'fit-content', padding: '0px' }} 
-                className="flex flex-row items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 max-h-20 w-2/6 bg-gradient-to-r from-primary via-secondary to-header rounded-xl border-header border-0" >
+                <div style={{  width: 'fit-content', padding: '0px' }} 
+                className="flex flex-row items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 max-h-20 w-2/6 " >
                   <img src="logo.png"         style={{
           width: '500px',
           height: 'auto',
           mixBlendMode: 'multiply', 
           height: '400px', 
-          border: '2px ',
+          border: '2px',
           padding: '10px', 
-          borderRadius: '15px',
+          borderRadius: '150px',
           overflow: 'hidden',
-          backgroundColor: 'bg-gradient-to-r from-secondary to-header rounded-xl border-header border-0'
+          backgroundImage: 'linear-gradient( #FA812F, #FAB12F)', 
+          display: 'flex',
         }}className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "></img>
                 </div>
       </main>
