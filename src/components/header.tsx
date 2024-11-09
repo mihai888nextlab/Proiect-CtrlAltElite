86e8be6bdc364b915da1ebe824b7047af1dd48bd
dragos1-5 <dragospopute@gmail.com>
2024-11-09 11:11:19 +0200

    Update header.tsx
    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
           <button className="font-semibold text-background px-4 py-2 rounded-lg mr-4 hover:bg-secondary" onClick={() => router.push("/login")}>
             Log In
           </button>
-          <button className="font-semibold bg-primary text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/signin")}>
+          <button className="font-semibold bg-primary text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/signup")}>
             Sign Up
           </button>
         </div>
