2b3f7049513d093e50c24f3dc4080b35a2ee1ec3
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-09 12:09:33 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,33 +15,33 @@ export default function Home() {
           <p className="font-semibold text-4xl my-4 mt-2 text-center text-background">Join us today and connect with like-minded individuals who share your passions.</p>
         </div>
         <div className="items-center justify-center flex flex-row">
-          <div className="min-h-20 w-2/5 mx-6  bg-gradient-to-r from-primary to-secondary rounded-xl border-header border-0">
+          <div className="min-h-20 w-2/5 mx-6 my-10 mt=20 bg-gradient-to-r from-primary to-secondary rounded-xl border-header border-0">
             <p className="font-semibold text-5xl my-5 text-center text-background">Why Join Social Sync?</p>
 
           </div>
-          <div className="w-2/5 min-h-20 mt-20 my=10 mx-6 bg-gradient-to-r from-secondary to-header rounded-xl border-header border-0">
+          <div className="w-2/5 min-h-20 mx-6 bg-gradient-to-r from-secondary to-header rounded-xl border-header border-0">
             <p className="font-semibold text-5xl my-5 text-center text-background">Ready to Connect?</p>
           </div>
         </div>
         <div className="items-center justify-center flex flex-row">
-          <div className="min-h-20 w-2/5 mx-6 bg-gradient-to-r h-[30rem] from-primary to-secondary rounded-xl border-header border-0">
+          <div className="w-2/5 my-5 mx-6 h-[30rem] bg-gradient-to-r  from-primary to-secondary rounded-xl border-header border-0">
             <p className="font-semibold text-3xl my-4 mx-4 text-center text-background">By joining Social Sync, you’ll gain access to:</p>
             <p className="font-semibold text-2xl my-3 mx-4 text-center text-background">-A Welcoming Community: Meet new people who are as passionate about your interests as you are.</p>
             <p className="font-semibold text-2xl my-3 mx-4 text-center text-background">-Exclusive Events: Participate in workshops, competitions, and hobby-related meetups that help you grow.</p>
             <p className="font-semibold text-2xl my-3 mx-4 text-center text-background">-Learn New Skills: Share knowledge and learn from others in a collaborative environment.</p>
             <p className="font-semibold text-2xl my-3 mx-4 text-center text-background">-Grow Your Network: Expand your social circle, collaborate with like-minded individuals, and create lasting friendships.</p>
           </div>
-          <div className="w-2/5 min-h-20 my-5 mx-6 bg-gradient-to-r h-[30rem] from-secondary to-header rounded-xl border-header border-0">
+          <div className="w-2/5 my-5 mx-6 h-[30rem] bg-gradient-to-r from-secondary to-header rounded-xl border-header border-0">
             <p className="font-semibold text-3xl my-28 mx-10 text-center text-background">Whether you’re looking to discover a new hobby, enhance your current skills, or make new friends, Social Sync is the place for you. Sign up today to join a growing community of passionate individuals from around the world.</p>
           </div>
           
         </div>
         
         <div className="items-center justify-center flex flex-row">
-          <button className="font-semibold w-1/5 min-h-20 my-24 mx-6 bg-gradient-to-r from-header via-secondary to-header rounded-xl text-3xl hover:from-secondary hover:via-header hover:to-secondary border-secondary border-2 font-semibold text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/signup")}>
+          <button className="-1/5 min-h-20 my-24 mx-6 bg-gradient-to-r from-header via-secondary to-header rounded-xl text-3xl hover:from-secondary hover:via-header hover:to-secondary border-secondary border-2 font-semibold text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/signup")}>
             Sign Up
           </button>
-          <button className="font-semibold w-1/5 min-h-20 my-24 mx-6 bg-gradient-to-r from-header via-secondary to-header rounded-xl text-3xl hover:from-secondary hover:via-header hover:to-secondary border-secondary border-2 font-semibold text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/login")}>
+          <button className="w-1/5 min-h-20 my-24 mx-6 bg-gradient-to-r from-header via-secondary to-header rounded-xl text-3xl hover:from-secondary hover:via-header hover:to-secondary border-secondary border-2 font-semibold text-background px-8 py-4 rounded-xl hover:bg-secondary" onClick={() => router.push("/login")}>
             Log In
           </button>
         </div>
