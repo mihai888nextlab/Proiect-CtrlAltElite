115d77e57bd709af79aabd0ba15e552655dc5ba8
CRISTISTG05 <cristi.stiegelbauer@gmail.com>
2024-11-09 06:30:13 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,8 +5,12 @@ export default function Home() {
     <div>
       <Header />
       <main className="min-h-screen bg-background pt-32">
-        <div className="py-4 mx-24 my-12 bg-gradient-to-r from-primary via-secondary to-header rounded-xl border-header border-4">
-            <h1 className="text-4xl mx-28 my-6 text-background">Welcome Social Sync</h1>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 max-h-20 w-2/6 bg-gradient-to-r from-primary via-secondary to-header rounded-xl border-header border-4">
+            <div className="text-4xl my-2 text-center text-background">Welcome to Social Sync</div>
+        </div>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto my-10 md:h-screen lg:py-0 w-10/12 bg-gradient-to-r from-primary via-secondary to-header rounded-xl border-header border-4">
+
+
         </div>
         
       </main>
