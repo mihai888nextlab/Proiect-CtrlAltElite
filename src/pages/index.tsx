e32497316ddf2bec485b9ec252867d8b0f168f0b
dragos1-5 <dragospopute@gmail.com>
2024-11-09 12:02:34 +0200

    sax
    hu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,15 @@ export default function Home() {
     <div>
       <Header />
 
-      <main className="bg-background min-h-screen"></main>
-
+      <main className="min-h-screen bg-background pt-28">
+                <div style={{ backgroundColor: 'orange', width: 'fit-content', padding: '10px' }} className="bg-gradient-to-r from-primary via-secondary to-header py-4 mx-24 my-20 rounded-xl border-header border-0">
+                  <img src="logo.png"         style={{
+          width: '500px',
+          height: 'auto',
+          mixBlendMode: 'multiply',
+        }}className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" ></img>
+                </div>
+      </main>
       <footer></footer>
     </div>
   );
