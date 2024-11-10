1553a3a9d288ad48cefe1ca9fa7a147aaefadef0
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2024-11-10 14:36:20 +0200

    fianl push doamne ajuta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import React, { useEffect } from 'react';
+import Header from "@/components/header";
 
-const MyComponent = () => {
-  useEffect(() => {
-      window.location.href = '/home';
-  }, []);
+export default function Index() {
+  return (
+    <div>
+      <Header />
 
-  return <div>Redirecting...</div>;
-};
-
-export default MyComponent;
+      <main>
+        <div
+          className="w-full h-screen flex flex-col items-center justify-center"
+          style={{
+            backgroundImage: "url('social.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="w-full h-full bg-black bg-opacity-50 flex flex-col items-center justify-center">
+            <h1 className="text-5xl font-bold text-secondary">
+              Welcome to <span className="font-bold text-white">Social</span>
+              <span className="font-bold text-primary">Sync</span>
+            </h1>
+            <p className="text-secondary mt-4 text-xl font-semibold">
+              A place to share your thoughts with the world
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
